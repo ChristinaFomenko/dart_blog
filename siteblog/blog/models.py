1d3c2c4dd8471bddd574cbec2bfb6f6d7d0a65ca
Christina <kris.inferno@gmail.com>
2021-05-25 14:01:44 +0300

    admin, settings

--- siteblog/blog/models.py
+++ siteblog/blog/models.py
@@ -23,7 +23,10 @@ class Category(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
         ordering = ['title']
+
 
 class Tag(models.Model):
     title = models.CharField(max_length=50)
@@ -33,21 +36,26 @@ class Tag(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = 'Категория(ю)'
+        verbose_name_plural = 'Категории'
         ordering = ['title']
 
+
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Наименование')
     slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
     author = models.CharField(max_length=100)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     views = models.IntegerField(default=0, verbose_name='Кол-во просмотров')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts', verbose_name='Категория')
     tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
 
     def __str__(self):
         return self.title
 
     class Meta:
+        verbose_name = 'Статья(ю)'
+        verbose_name_plural = 'Статьи'
         ordering = ['-created_at']
