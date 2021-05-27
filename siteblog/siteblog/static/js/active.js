c3d5fe73980f10378ec49e7250ce19c820e07616
Christina <kris.inferno@gmail.com>
2021-05-27 16:52:55 +0300

    menu template tag

--- siteblog/siteblog/static/js/active.js
+++ siteblog/siteblog/static/js/active.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         if (location == link){
             $(this).parent().addClass('active');
         }
-        });
+    });
 
     var $grid = $('.grid').isotope({
         layoutMode: 'fitRows'
