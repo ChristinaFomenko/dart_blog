47ba8a7a66a629dade55eaf43658df9db5aa78e0
Christina <kris.inferno@gmail.com>
2021-06-01 15:14:01 +0300

    search, tags, popular_posts

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import *
+from .models import Post, Category, Tag
 from django.db.models import F
 
 
@@ -31,8 +31,19 @@ class PostsByCategory(ListView):
         return context
 
 
-class PostsByTag(ListView):
-    pass
+class PostsByTag(ListView):    #вывод постов по тегу
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Записи по тегу: ' + str(Tag.objects.get(slug=self.kwargs['slug']))
+        return context
 
 
 class GetPost(DetailView):
@@ -48,4 +59,15 @@ class GetPost(DetailView):
         return context
 
 
+class Search(ListView):
+    template_name = 'blog/search.html'
+    context_object_name = 'posts'
+    paginate_by = 2
 
+    def get_queryset(self):
+        return Post.objects.filter(title__icontains=self.request.GET.get('s'))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['s'] = f"s={self.request.GET.get('s')}&"
+        return context
